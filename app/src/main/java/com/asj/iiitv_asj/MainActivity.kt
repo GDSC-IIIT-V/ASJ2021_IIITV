c6f46b29829db2b1dfc3b25a085e80d858e24d4b
AmanKothari20 <amankothari123456@gmail.com>
2022-01-08 17:24:05 +0530

    firebase added

--- app/src/main/java/com/asj/iiitv_asj/MainActivity.kt
+++ app/src/main/java/com/asj/iiitv_asj/MainActivity.kt
@@ -2,10 +2,12 @@ package com.asj.iiitv_asj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
